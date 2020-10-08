6053f4fd3b3bafcd3a85d4c49487a68148bbdcca
leosunny67 <43566194+leosunny67@users.noreply.github.com>
2020-10-08 13:48:04 +0530

    uploads

--- App.js
+++ App.js
@@ -1,75 +1,25 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
 
-import Header from './components/Header'
-import Gamescreen from './screens/Gamescreen';
-import Startgamescreen from './screens/Startgamescreen'
-import Gameover from './screens/Gameover'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import TodoApp from './src/TodoApp';
+import store from './src/store'
+import { Provider } from 'react-redux'
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-};
-
-export default function App() {
-
-  const [usernumber, setusernumber] = useState()
-
-  const [guessround, setguessround] = useState(0)
-
-  const [dataloaded, setdataloaded] = useState(false)
-
-  if (!dataloaded) {
+export default class App extends React.Component {
+  render() {
     return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setdataloaded(true)}
-        onError={(err) => console.log(err)}
-      />
+      <Provider store={store}>
+        <TodoApp />
+      </Provider>
     );
   }
-
-  const confignewgame = () => {
-    setguessround(0);
-    setusernumber(null)
-  }
-
-  const startgamehandler = (selectednumber) => {
-    setusernumber(selectednumber)
-    setguessround(0)
-  }
-
-  const gameoverhandler = numofrounds => {
-    setguessround(numofrounds)
-  }
-
-  let content = <Startgamescreen onStartgame={startgamehandler} />
-
-  if (usernumber && guessround <= 0) {
-    content = <Gamescreen userChoice={usernumber} onGameover={gameoverhandler} />
-  }
-  else if (guessround > 0) {
-    content = <Gameover roundsnumber={guessround} usernumber={usernumber} onrestart={confignewgame} />
-  }
-
-
-  return (
-    <View style={styles.container}>
-
-      <Header title='Guess a Nummber' />
-      {content}
-
-    </View>
-    )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
-  }
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
